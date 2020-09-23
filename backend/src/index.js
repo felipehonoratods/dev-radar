bd48c5d1cefe643a9dc58bf04d3f9adf9a2baeb4
Felipe Honorato <felipehonoratods@gmail.com>
2020-09-23 12:45:48 -0300

    build: Creation of backend features

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://felipe:99785867@cluster0.ne68r.mongodb.net/devRadar?retryWrites=true&w=majority', {
+  useNewUrlParser: true,  
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
