7079ea32ad50781a4c49e785a68afab145de1e3f
Felipe Honorato <felipehonoratods@gmail.com>
2020-10-13 18:11:54 -0300

    feat: adding new features from socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://felipe:<password>@cluster0.ne68r.mongodb.net/devRadar?retryWrites=true&w=majority', {
   useNewUrlParser: true,  
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -25,14 +26,30 @@ function Main({ navigation }) {
         setCurrentRegion({
           latitude,
           longitude,
-          latitudeDelta: 0.04,
-          longitudeDelta: 0.04
+          latitudeDelta: 0.10,
+          longitudeDelta: 0.10
         });
       }
     }
 
     loadInitialPosition();
   }, []);
+
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebSocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+    
+    connect(
+      latitude, 
+      longitude,
+      techs
+    );
+  }
 
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
@@ -46,6 +63,7 @@ function Main({ navigation }) {
     });
 
     setDevs(response.data.devs);
+    setupWebSocket();
   }
 
   function handleRegionChanged(region) {
